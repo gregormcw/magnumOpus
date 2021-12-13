89af35b553181b753c21e932f886fc0f53527233
Gregor McWilliam <gregormcwilliam@mcdubs-mbp.lan>
2021-12-13 00:34:08 -0500

    Updated GUI

--- client/Source/MainComponent.cpp
+++ client/Source/MainComponent.cpp
@@ -8,29 +8,177 @@ MainComponent::MainComponent()
 {
     setSize (WIN_WIDTH, WIN_HEIGHT);
     
-//    aziSlider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
-//    aziSlider.setRange(-180.0f, 180.0f, 1.0f);
-//    aziSlider.setValue(0.0f);
-//    aziSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 20);
-//    aziSlider.addListener(this);
-//    addAndMakeVisible(aziSlider);
-//
     volSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
-    volSlider.setRange(0.0f, 100.0f, 1.0f);
-    volSlider.setValue(70.0f);
+    volSlider.setRange(0.0f, 1.0f, 0.02f);
+    volSlider.setValue(0.7f);
     volSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 20);
-//    volSlider.addListener(this);
+    volSlider.onValueChange = [this] { volume = volSlider.getValue(); };
     addAndMakeVisible(volSlider);
     
-    addAndMakeVisible(buttonMono);
+    // ===================================================
+    // CHANGE LAMBDA FUNCTION CALLS - CURRENTLY ALL PLAY()
+    // ===================================================
+    
+    buttonConnect.setToggleState(false, juce::NotificationType::dontSendNotification);
+    buttonConnect.onClick = [this]() { play(); };
+    buttonConnect.addListener(this);
+    
+    buttonStereo.setToggleState(false, juce::NotificationType::dontSendNotification);
+    buttonStereo.onClick = [this]() { play(); };
+    buttonStereo.addListener(this);
+    
+    buttonBinaural.setToggleState(false, juce::NotificationType::dontSendNotification);
+    buttonBinaural.onClick = [this]() { play(); };
+    buttonBinaural.addListener(this);
+    
+    button5_1.setToggleState(false, juce::NotificationType::dontSendNotification);
+    button5_1.onClick = [this]() { play(); };
+    button5_1.addListener(this);
+
     addAndMakeVisible(buttonStereo);
     addAndMakeVisible(button5_1);
-    addAndMakeVisible(button7_1);
+    addAndMakeVisible(buttonBinaural);
     
+    buttonPlay.setToggleState(false, juce::NotificationType::dontSendNotification);
+    buttonPlay.onClick = [this]() { play(); };
+    buttonPlay.addListener(this);
+    
+    buttonMute.setToggleState(false, juce::NotificationType::dontSendNotification);
+    buttonMute.onClick = [this]() { mute(); };
+    buttonMute.addListener(this);
+    
+    buttonStop.setToggleState(false, juce::NotificationType::dontSendNotification);
+    buttonStop.onClick = [this]() { stop(); };
+    buttonStop.addListener(this);
+    
+    addAndMakeVisible(buttonConnect);
     addAndMakeVisible(buttonPlay);
     addAndMakeVisible(buttonMute);
     addAndMakeVisible(buttonStop);
+    
 }
+
+void MainComponent::play() {
+    
+    if (playState == PlayState::Stop) {
+        playState = PlayState::Play;
+        
+//        edit.getTransport().play(false);
+//        juce::Timer::startTimer(100);
+        
+    }
+}
+
+void MainComponent::mute() {
+    
+    if (playState == PlayState::Play) {
+        playState = PlayState::Mute;
+    
+    }
+}
+
+void MainComponent::stop() {
+    
+    if (playState == PlayState::Play) {
+        playState = PlayState::Stop;
+        
+    }
+}
+
+void MainComponent::buttonClicked(juce::Button* button) {
+    
+    if (button == &buttonConnect) {
+        if (!isConnected) {
+            buttonConnect.setColour(0x1000100, juce::Colours::orange);
+        }
+        else {
+            buttonConnect.setColour(0x1000100, juce::Colours::black);
+        }
+        isConnected = !isConnected;
+    }
+    
+    if (button == &buttonStereo) {
+        
+        playbackFormat = 0;
+//        std::cout << "Format: Stereo" << std::endl;
+        
+        isStereo = true;
+        isBinaural = false;
+        is5_1 = false;
+        
+        buttonStereo.setColour(0x1000100, juce::Colours::orange);
+        buttonBinaural.setColour(0x1000100, juce::Colours::black);
+        button5_1.setColour(0x1000100, juce::Colours::black);
+    }
+    
+    if (button == &buttonBinaural) {
+        
+        playbackFormat = 1;
+//        std::cout << "Format: Binaural" << std::endl;
+        
+        isStereo = false;
+        isBinaural = true;
+        is5_1 = false;
+        
+        buttonBinaural.setColour(0x1000100, juce::Colours::orange);
+        buttonStereo.setColour(0x1000100, juce::Colours::black);
+        button5_1.setColour(0x1000100, juce::Colours::black);
+    }
+    
+    if (button == &button5_1) {
+        
+        playbackFormat = 2;
+//        std::cout << "Format: 5.1" << std::endl;
+        
+        isStereo = false;
+        isBinaural = false;
+        is5_1 = true;
+        
+        button5_1.setColour(0x1000100, juce::Colours::orange);
+        buttonBinaural.setColour(0x1000100, juce::Colours::black);
+        buttonStereo.setColour(0x1000100, juce::Colours::black);
+    }
+    
+    if (button == &buttonPlay) {
+        
+        isPlaying = !isPlaying;
+        
+        buttonPlay.setColour(0x1000100, juce::Colours::limegreen);
+        buttonStop.setColour(0x1000100, juce::Colours::whitesmoke);
+        buttonStop.setColour(0x1000102, juce::Colours::black);
+        buttonPlay.colourChanged();
+        buttonStop.colourChanged();
+    }
+    
+    if (button == &buttonMute) {
+        if (!isMuted) {
+            buttonMute.setColour(0x1000100, juce::Colours::indianred);
+            buttonMute.setColour(0x1000102, juce::Colours::whitesmoke);
+        }
+        else {
+            buttonMute.setColour(0x1000100, juce::Colours::whitesmoke);
+            buttonMute.setColour(0x1000102, juce::Colours::black);
+        }
+        isMuted = !isMuted;
+        buttonMute.colourChanged();
+    }
+    
+    if (button == &buttonStop) {
+        
+        isPlaying = false;
+        
+        buttonStop.setColour(0x1000100, juce::Colours::indianred);
+        buttonStop.setColour(0x1000102, juce::Colours::whitesmoke);
+        buttonPlay.setColour(0x1000100, juce::Colours::whitesmoke);
+        buttonStop.colourChanged();
+        buttonPlay.colourChanged();
+    }
+}
+
+//void sliderValueChanged(Slider* slider) override {
+//    if (slider == &volSlider)
+//        volSlider.setValue(volSlider)
+//}
 
 MainComponent::~MainComponent()
 {
@@ -68,47 +216,60 @@ void MainComponent::resized()
     // If you add any child components, this is where you should
     // update their positions.
     
-    int buttonWidth = 240;
-    int buttonHeight = 144;
+    int connectButtonWidth = 240*1.6;
+    int connectButtonHeight = 144*0.9;
     
-    int ctlButtonWidth = 4*buttonWidth/5;
-    int ctlButtonHeight = 4*buttonHeight/5;
+    int selectionButtonWidth = 240*1;
+    int selectionButtonHeight = 144*0.9;
     
-    int border = buttonWidth / 12;
+    int ctlButtonWidth = 4*selectionButtonWidth/5;
+    int ctlButtonHeight = 4*selectionButtonHeight/5;
     
-    buttonMute.setColour(0x1000100, juce::Colours::orange);
-    buttonPlay.setColour(0x1000100, juce::Colours::orange);
-    buttonStop.setColour(0x1000100, juce::Colours::orange);
+    int border = selectionButtonWidth / 12;
+    
+    buttonStereo.setColour(0x1000100, juce::Colours::black);
+    buttonBinaural.setColour(0x1000100, juce::Colours::black);
+    button5_1.setColour(0x1000100, juce::Colours::black);
+    
+    buttonConnect.setColour(0x1000100, juce::Colours::black);
+    buttonMute.setColour(0x1000100, juce::Colours::whitesmoke);
+    buttonMute.setColour(0x1000102, juce::Colours::black);
+    buttonPlay.setColour(0x1000100, juce::Colours::whitesmoke);
+    buttonPlay.setColour(0x1000102, juce::Colours::black);
+    buttonStop.setColour(0x1000100, juce::Colours::whitesmoke);
+    buttonStop.setColour(0x1000102, juce::Colours::black);
     
 //    buttonMono.setState(juce::Button::buttonDown);
     
-    buttonMute.setBounds(getWidth()/2 - 1.5*ctlButtonWidth - border, getHeight()/2 + ctlButtonHeight/2,
+    buttonMute.setBounds(getWidth()/2 - 1.5*ctlButtonWidth - 1.36*border, getHeight()/2 + 1.24*ctlButtonHeight,
                          ctlButtonWidth, ctlButtonHeight);
     
-    buttonPlay.setBounds(getWidth()/2 - ctlButtonWidth/2, getHeight()/2 + ctlButtonHeight/2,
+    buttonPlay.setBounds(getWidth()/2 - ctlButtonWidth/2, getHeight()/2 + 1.24*ctlButtonHeight,
                           ctlButtonWidth, ctlButtonHeight);
     
-    buttonStop.setBounds(getWidth()/2 + ctlButtonWidth/2 + border, getHeight()/2 + ctlButtonHeight/2,
+    buttonStop.setBounds(getWidth()/2 + ctlButtonWidth/2 + 1.36*border, getHeight()/2 + 1.24*ctlButtonHeight,
                          ctlButtonWidth, ctlButtonHeight);
     
+    buttonConnect.setBounds(getWidth()/2 - connectButtonWidth/2, getHeight()/4 + connectButtonHeight + 42,
+                            connectButtonWidth, connectButtonHeight);
     
+    // ====================================================================================
+    // ====================================================================================
+    // ====================================================================================
     
-    buttonMono.setBounds(getWidth()/5 - buttonWidth/2 - border, getHeight()/4 + buttonHeight/2,
-                         buttonWidth, buttonHeight);
+    buttonStereo.setBounds(getWidth()/2 - 1.5*selectionButtonWidth - 4*border, getHeight()/4 + selectionButtonHeight/5,
+                           selectionButtonWidth, selectionButtonHeight);
     
-    buttonStereo.setBounds(2*getWidth()/5 - buttonWidth/2 - border/3, getHeight()/4 + buttonHeight/2,
-                           buttonWidth, buttonHeight);
+    buttonBinaural.setBounds(getWidth()/2 - selectionButtonWidth/2, getHeight()/4 + selectionButtonHeight/5,
+                        selectionButtonWidth, selectionButtonHeight);
     
-    button5_1.setBounds(3*getWidth()/5 - buttonWidth/2 + border/3, getHeight()/4 + buttonHeight/2,
-                        buttonWidth, buttonHeight);
-    
-    button7_1.setBounds(4*getWidth()/5 - buttonWidth/2 + border, getHeight()/4 + buttonHeight/2,
-                        buttonWidth, buttonHeight);
+    button5_1.setBounds(getWidth()/2 + selectionButtonWidth/2 + 4*border, getHeight()/4 + selectionButtonHeight/5,
+                        selectionButtonWidth, selectionButtonHeight);
     
     int volSliderWidth = 440;
     int volSliderHeight = 100;
     
-    volSlider.setBounds(getWidth()/2 - volSliderWidth/2, getHeight()/2 + 200,
+    volSlider.setBounds(getWidth()/2 - volSliderWidth/2, getHeight()/2 + 280,
                             volSliderWidth, volSliderHeight);
 
 }
--- client/Source/MainComponent.h
+++ client/Source/MainComponent.h
@@ -7,30 +7,68 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  :  public juce::Component,
+                        public juce::Button::Listener
+//                        public juce::Slider::Listener
+//                        public juce::Slider::Listener
+//                        public juce::Timer,
+//                        public juce::Button::Listener
 {
 public:
     //==============================================================================
     MainComponent();
     ~MainComponent() override;
-
+    
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    int volume = 0.7;
+    
+    void play();
+    void stop();
+    void mute();
+//    void timerCallback() override;
+    void buttonClicked(juce::Button* button) override;
+    
+//
+//    void sliderValueChanged(juce::Slider* slider) override;
 
 private:
     //==============================================================================
     // Your private member variables go here...
     
+    enum class PlayState {
+        Play,
+        Stop,
+        Mute
+    };
+    
+    PlayState playState {PlayState::Stop};
+    
+    juce::Atomic<int> playbackFormat = 0;
+    bool isConnected = false;
+    
+    bool isStereo = false;
+    bool isBinaural = false;
+    bool is5_1 = false;
+    
+//    juce::Atomic<bool> isStereo = true;
+//    juce::Atomic<bool> isBinaural = false;
+//    juce::Atomic<bool> is5_1 = false;
+    
+    bool isPlaying = false;
+    bool isMuted = false;
+    
+    juce::TextButton buttonConnect{"CONNECT"};
     juce::TextButton buttonPlay{"PLAY"};
     juce::TextButton buttonMute{"MUTE"};
     juce::TextButton buttonStop{"STOP"};
     juce::Slider volSlider;
     
-    juce::TextButton buttonMono{"MONO"};
     juce::TextButton buttonStereo{"STEREO"};
+    juce::TextButton buttonBinaural{"BINAURAL"};
     juce::TextButton button5_1{"5.1"};
-    juce::TextButton button7_1{"7.1"};
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
